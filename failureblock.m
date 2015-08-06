d3b3d411c210100d8ef8cb50f4c66ffb8c5389c5
Stefan Herold <stherold@Stefans-MacBook-Pro.local>
2015-08-06 15:54:41 +0200

    removed duplicated snippets

--- failureblock.m
+++
@@ -1,9 +0,0 @@
-// failureBlock
-// 
-//
-// IDECodeSnippetCompletionScopes: [All]
-// IDECodeSnippetIdentifier: 4E141399-0E86-4DB6-A4D5-D46057E71E29
-// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
-// IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 2
-failureBlock:[self errorBlock:expectation] errorBlock:[self errorBlock:expectation]];
